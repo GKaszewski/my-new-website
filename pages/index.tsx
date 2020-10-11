20b620d0f768d2e8755d5107528658420548cb15
Gabriel <GABKASZ47@edu.gdansk.pl>
2020-10-11 02:45:25 +0200

    Boilerplate site

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import { Button, Typography } from "@material-ui/core";
+import Head from "next/head";
+export default function Home() {
+	return (
+		<div>
+			<Head>
+				<title>Gabriel Kaszewski</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<Typography variant="h1">Gabriel Kaszewski</Typography>
+		</div>
+	);
+}
