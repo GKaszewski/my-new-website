f659c024954d930235144902b76504d48235b1ac
Gabriel <GABKASZ47@edu.gdansk.pl>
2020-10-22 22:53:26 +0200

    Project component v1

--- src/components/projectPanel.tsx
+++ src/components/projectPanel.tsx
@@ -3,10 +3,12 @@ import {
 	Button,
 	Chip,
 	Container,
+	createStyles,
 	Grid,
 	Grow,
 	makeStyles,
 	Paper,
+	Theme,
 	Typography,
 } from "@material-ui/core";
 import React from "react";
@@ -16,26 +18,33 @@ interface Props {
 	project: Project;
 }
 
-const useStyles = makeStyles({
-	root: {
-		height: "100%",
-	},
-	imgContainer: {
-		width: "100%",
-		height: "100%",
-		"& img": {
-			display: "block",
-			objectFit: "cover",
-			height: "auto",
-			width: "100%",
+const useStyles = makeStyles((theme: Theme) =>
+	createStyles({
+		root: {
+			height: "100%",
 		},
-	},
-	description: {
-		hyphens: "auto",
-		wordWrap: "normal",
-		whiteSpace: "pre-line",
-	},
-});
+		imgContainer: {
+			width: "100%",
+			height: "100%",
+			"& img": {
+				display: "block",
+				objectFit: "cover",
+				height: "auto",
+				width: "100%",
+				marginTop: "1rem",
+				marginBottom: "1rem",
+			},
+		},
+		description: {
+			hyphens: "auto",
+			wordWrap: "normal",
+			whiteSpace: "pre-line",
+		},
+		chip: {
+			color: theme.palette.text.primary,
+		},
+	})
+);
 
 export default function ProjectPanel(props: Props) {
 	const classes = useStyles();
@@ -49,7 +58,7 @@ export default function ProjectPanel(props: Props) {
 					justify="space-evenly"
 					spacing={2}
 				>
-					<Grid item xs>
+					<Grid item xs={12} sm={6}>
 						<Grid
 							container
 							direction="column"
@@ -65,15 +74,30 @@ export default function ProjectPanel(props: Props) {
 									{props.project.description}
 								</Typography>
 							</Grid>
-							<Grid item>
-								<Chip color="primary" label={props.project.technology} />
-							</Grid>
+							{props.project.technology ? (
+								<Grid item>
+									<Grid container direction="row" spacing={1}>
+										{props.project.technology.map((tech) => {
+											return (
+												<Grid key={tech} item>
+													<Chip
+														//className={classes.chip}
+														color="secondary"
+														label={tech}
+													/>{" "}
+												</Grid>
+											);
+										})}
+									</Grid>
+								</Grid>
+							) : null}
+
 							<Grid item>
 								<Grid container direction="row" spacing={1}>
 									{props.project.githubUrl ? (
 										<Grid item>
 											<Button
-												color="primary"
+												color="secondary"
 												variant="outlined"
 												href={props.project.githubUrl}
 											>
@@ -85,7 +109,7 @@ export default function ProjectPanel(props: Props) {
 									{props.project.visitUrl ? (
 										<Grid item>
 											<Button
-												color="primary"
+												color="secondary"
 												variant="outlined"
 												href={props.project.visitUrl}
 											>
@@ -97,7 +121,7 @@ export default function ProjectPanel(props: Props) {
 									{props.project.downloadUrl ? (
 										<Grid item>
 											<Button
-												color="primary"
+												color="secondary"
 												variant="outlined"
 												href={props.project.downloadUrl}
 											>
@@ -109,12 +133,21 @@ export default function ProjectPanel(props: Props) {
 							</Grid>
 						</Grid>
 					</Grid>
-					<Grid item xs>
-						<Container className={classes.imgContainer}>
+					{/* {props.project.thumbnailUrls ? (
+						<Grid item xs>
+							<Container className={classes.imgContainer}>
+								<Paper elevation={3}>
+									<img src={props.project.thumbnailUrls[0]}></img>
+								</Paper>
+							</Container>
+						</Grid>
+					) : null} */}
+					<Grid item xs={12} sm={6}>
+						<Box className={classes.imgContainer}>
 							<Paper elevation={3}>
-								<img src="https://i.imgur.com/rSPt0qi.jpg"></img>
+								<img src="https://i.imgur.com/MNLefuT.jpg"></img>
 							</Paper>
-						</Container>
+						</Box>
 					</Grid>
 				</Grid>
 			</Container>
