196bc7e1d825719d64c39e19a81ce86fd1a703a6
Gabriel <GABKASZ47@edu.gdansk.pl>
2020-10-29 14:36:44 +0100

    Added projects page

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -4,40 +4,13 @@ import ProjectPanel from "../src/components/projectPanel";
 import React, { useEffect } from "react";
 import fetchProjects from "../src/redux/dispatchers/projects/fetchProjects";
 import { Project } from "../src/redux/types";
-import NavigationBar from "../src/components/navigationbar";
 import {
-	Box,
-	Container,
-	createStyles,
-	IconButton,
-	makeStyles,
-	Theme,
+	Grid,
 	Typography,
 } from "@material-ui/core";
-import Footbar from "../src/components/footbar";
 import Spinner from "../src/components/spinner";
+import { BaseLayout } from "../src/components/baselayout";
 
-const useStyles = makeStyles((theme: Theme) =>
-	createStyles({
-		root: {
-			display: "flex",
-			flexFlow: "column wrap",
-			minHeight: "100vh",
-		},
-		content: {
-			display: "flex",
-			flexFlow: "column wrap",
-			alignItems: "center",
-			justifyContent: "space-evenly",
-		},
-		filler: {
-			flexGrow: 1,
-		},
-		footer: {
-			marginTop: "auto",
-		},
-	})
-);
 
 export default function Projects() {
 	const dispatch = useDispatch();
@@ -46,27 +19,25 @@ export default function Projects() {
 		dispatch(fetchProjects());
 	}, []);
 
-	const classes = useStyles();
-
 	return (
-		<div>
-			<Head>
-				<title>Gabriel Kaszewski - Projects</title>
-			</Head>
-			<Box className={classes.root}>
-				<NavigationBar />
-				<Box className={classes.content}>
-					<Typography variant="h2">My projects</Typography>
-					{projects.map((project: Project) => {
-						return (
-							<ProjectPanel key={`project-${project.name}`} project={project} />
-						);
-					})}
-				</Box>
-				<Spinner open={pending} />
-				<Box className={classes.filler} />
-				<Footbar />
-			</Box>
-		</div>
+		<BaseLayout title="Gabriel Kaszewski - Projects">
+			<Grid item>
+				<Grid container direction="column" justify="center" alignContent="center" alignItems="center" spacing={2}>
+					<Grid item>
+						<Typography variant="h3">My projects</Typography>
+					</Grid>
+					<Grid item>
+						{projects.map((project: Project) => {
+							return (
+								<ProjectPanel key={`project-${project.name}`} project={project} />
+							);
+						})}
+					</Grid>
+					<Grid item>
+						<Spinner open={pending} />
+					</Grid>
+				</Grid>
+			</Grid>
+		</BaseLayout>
 	);
 }
