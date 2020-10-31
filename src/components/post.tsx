ffa4e542634a5d2e96d56781c7b1c41c1c0b55ba
Gabriel <GABKASZ47@edu.gdansk.pl>
2020-10-31 03:30:35 +0100

    Added text styling

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -4,11 +4,19 @@ import gfm from 'remark-gfm';
 import { Post } from '../redux/types';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { dark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
-import { Grid, Typography } from '@material-ui/core';
+import { Grid, makeStyles, Typography } from '@material-ui/core';
 
 interface Props {
     data: Post
 }
+
+const useStyles = makeStyles({
+    content: {
+        textAlign: 'justify',
+        '& :link': { textDecoration: 'none' },
+        '& :visited': { color: 'blue' }
+    }
+});
 
 const renderers = {
     code: ({ language, value }) => {
@@ -17,13 +25,14 @@ const renderers = {
 }
 
 export default function PostComponent(props: Props) {
+    const classes = useStyles();
     return <Grid item container direction="column">
         <Grid item xs={12}>
             <Typography variant="h3">{props.data.title}</Typography>
             <Typography variant="h6">{new Date(props.data.created_on).toDateString()}</Typography>
         </Grid>
         <Grid item xs={12}>
-            <ReactMarkdown plugins={[gfm]} renderers={renderers}>
+            <ReactMarkdown className={classes.content} plugins={[gfm]} renderers={renderers}>
                 {props.data.content}
             </ReactMarkdown>
         </Grid>
