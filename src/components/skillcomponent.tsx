c901636c721c0280e8d3992f780faa85b5a2cc4e
Gabriel <GABKASZ47@edu.gdansk.pl>
2020-11-05 16:35:14 +0100

    Changed color

--- src/components/skillcomponent.tsx
+++ src/components/skillcomponent.tsx
@@ -1,20 +1,11 @@
 import React from 'react';
-import { Chip, createStyles, makeStyles, Theme } from '@material-ui/core';
+import { Chip } from '@material-ui/core';
 import { Skill } from '../redux/types';
 
 interface Props {
     skill: Skill;
 }
 
-const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        chip: {
-            color: theme.palette.text.primary,
-        },
-    })
-);
-
 export default function SkillComponent(props: Props) {
-    const classes = useStyles();
-    return <Chip className={classes.chip} color="primary" label={props.skill.name} />
+    return <Chip color="secondary" label={props.skill.name} />
 }
