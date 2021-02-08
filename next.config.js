4c91d0b2b430815cecb29287c064ecd8298bdd89
Gabriel <GABKASZ47@edu.gdansk.pl>
2021-02-08 17:26:59 +0100

    Fixes

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  images: {
+    domains: [
+      "i.imgur.com",
+      "1893713375.rsc.cdn77.org",
+      "user-images.githubusercontent.com",
+    ],
+  },
+  target: "serverless",
+};
