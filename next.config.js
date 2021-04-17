635a5bb41cdf6e466e666c28839ba935fac25871
Gabriel <gabrielkaszewski@gmail.com>
2021-04-17 21:44:58 +0200

    config for vps

--- next.config.js
+++ next.config.js
@@ -2,9 +2,8 @@ module.exports = {
   images: {
     domains: [
       "i.imgur.com",
-      "1893713375.rsc.cdn77.org",
       "user-images.githubusercontent.com",
     ],
   },
-  target: "serverless",
+  //target: "serverless",
 };
