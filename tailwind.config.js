201535a181b8e80f775efcf005d738a13cbaec2e
Gabriel <gabrielkaszewski@gmail.com>
2021-05-03 19:46:09 +0200

    Typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,39 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme('colors.gray.100'),
+            h1: {
+              color: theme('colors.gray.100'),
+            },
+            h2: {
+              color: theme('colors.gray.100'),
+            },
+            h3: {
+              color: theme('colors.gray.100'),
+            },
+            strong: {
+              color: theme('colors.gray.100'),
+              fontWeight: theme('fontWeight.bold'),
+            },
+            a: {
+              color: theme('colors.green.500'),
+              '&:hover': {
+                color: theme('colors.green.600')
+              },
+            },
+          },
+        },
+      })
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 };
