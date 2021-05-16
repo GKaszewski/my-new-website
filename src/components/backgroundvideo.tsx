0c937c16f8cc7111ebf5f6133fed8f08b2bd92dc
Gabriel <gabrielkaszewski@gmail.com>
2021-05-16 23:47:18 +0200

    background video upgrade!

--- src/components/backgroundvideo.tsx
+++ src/components/backgroundvideo.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import styles from "./BackgroundVideo.module.css";
 
 interface Props {
   text?: string;
@@ -8,9 +7,9 @@ interface Props {
 
 export default function BackgroundVideoComponent(props: Props) {
   return (
-    <div className={"min-w-full w-full"}>
+    <div className={"min-w-full w-full relative inline-block"}>
       <video
-        className="relative max-h-full w-full object-cover pointer-events-none"
+        className="max-h-full w-full object-cover pointer-events-none"
         autoPlay
         muted
         playsInline
@@ -20,7 +19,9 @@ export default function BackgroundVideoComponent(props: Props) {
         <source src={props.source} type="video/webm" />
       </video>
       <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
-        <h1 className="text-5xl text-white">{props.text}</h1>
+        <h1 className="text-lg md:text-2xl lg:text-3xl xl:text-5xl 2xl:text-9xl text-white tracking-wide uppercase shadow">
+          {props.text}
+        </h1>
       </div>
     </div>
   );
