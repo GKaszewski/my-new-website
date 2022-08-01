6372c75828dab9e6e254b031da59752f3f0cf698
Gabriel Kaszewski <gabrielkaszewski@gmail.com>
2022-08-01 04:13:27 +0200

    Upgrade tailwind to V3 and remove old files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,43 +1,37 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      typography: (theme) => ({
-        DEFAULT: {
-          css: {
-            color: theme('colors.gray.100'),
-            h1: {
-              color: theme('colors.gray.100'),
-            },
-            h2: {
-              color: theme('colors.gray.100'),
-            },
-            h3: {
-              color: theme('colors.gray.100'),
-            },
-            strong: {
-              color: theme('colors.gray.100'),
-              fontWeight: theme('fontWeight.bold'),
-            },
-            code: {
-              color: theme('colors.green.500')
-            },
-            a: {
-              color: theme('colors.green.500'),
-              '&:hover': {
-                color: theme('colors.green.600')
-              },
-            },
-          },
-        },
-      })
-    },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
+	content: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			typography: (theme) => ({
+				DEFAULT: {
+					css: {
+						color: theme("colors.gray.100"),
+						h1: {
+							color: theme("colors.gray.100"),
+						},
+						h2: {
+							color: theme("colors.gray.100"),
+						},
+						h3: {
+							color: theme("colors.gray.100"),
+						},
+						strong: {
+							color: theme("colors.gray.100"),
+							fontWeight: theme("fontWeight.bold"),
+						},
+						code: {
+							color: theme("colors.green.500"),
+						},
+						a: {
+							color: theme("colors.green.500"),
+							"&:hover": {
+								color: theme("colors.green.600"),
+							},
+						},
+					},
+				},
+			}),
+		},
+	},
+	plugins: [require("@tailwindcss/typography")],
 };
