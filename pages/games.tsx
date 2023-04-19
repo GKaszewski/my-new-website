1ea8e601bdc6118fd272eb8eaec3093699356e38
Gabriel Kaszewski <gabrielkaszewski@gmail.com>
2023-04-19 18:18:19 +0200

    Update urls

--- pages/games.tsx
+++ pages/games.tsx
@@ -4,9 +4,9 @@ import { Button } from "../src/components/button";
 
 const unityContext = new UnityContext({
 	loaderUrl: "https://gabrielkaszewski.dev/media/Builds.loader.js",
-	dataUrl: "https://gabrielkaszewski.dev/media/Builds.data.gz",
-	frameworkUrl: "https://gabrielkaszewski.dev/media/Builds.framework.js.gz",
-	codeUrl: "https://gabrielkaszewski.dev/media/Builds.wasm.gz",
+	dataUrl: "https://gabrielkaszewski.dev/media/Builds.data",
+	frameworkUrl: "https://gabrielkaszewski.dev/media/Builds.framework.js",
+	codeUrl: "https://gabrielkaszewski.dev/media/Builds.wasm",
 });
 
 const GamesPage = () => {
