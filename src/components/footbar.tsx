ae5ce034717015dfb465b7f5b476115accc9e1dc
Gabriel Kaszewski <gabrielkaszewski@gmail.com>
2023-12-03 20:19:24 +0100

    Update blog

--- src/components/footbar.tsx
+++ src/components/footbar.tsx
@@ -4,55 +4,38 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 export default function Footbar() {
-	const { loggedIn } = useSelector((state) => state.authReducer);
+  const { loggedIn } = useSelector((state) => state.authReducer);
 
-	return (
-		<div className="flex flex-col bg-gray-900 w-full p-4 gap-4">
-			<div className="flex gap-2">
-				<h1 className="text-xl">Gabriel Kaszewski</h1>
-				<span className="flex-1" />
-				<a href="https://github.com/GKaszewski">
-					<FontAwesomeIcon icon={["fab", "github"]} />
-				</a>
-				<a href="https://www.linkedin.com/in/gabriel-kaszewski-5344b3183">
-					<FontAwesomeIcon icon={["fab", "linkedin-in"]} />
-				</a>
-			</div>
-			<div className="flex gap-2 text-sm">
-				<p className="font-semibold">
-					© Gabriel Kaszewski, {new Date().getFullYear()}. All rights
-					reserved.
-				</p>
-				<span className="flex-1" />
-				{loggedIn && (
-					<a href="https://gabrielkaszewski.dev/api/admin">
-						Admin page
-					</a>
-				)}
-				<Link href="/projects">
-					Projects
-				</Link>
-				<Link href="/blog">
-					Blog
-				</Link>
-				{loggedIn && (
-					<Link href="/blog/editor">
-						Editor
-					</Link>
-				)}
-				{loggedIn && (
-					<Link href="/files">
-						Files
-					</Link>
-				)}
-				<Link href="/contact">
-					Contact
-				</Link>
-				<Link href="/login">
-					Login
-				</Link>
-				<a href="http://media.gabrielkaszewski.dev/">Media</a>
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex flex-col w-full gap-4 p-4 bg-gray-900">
+      <div className="flex gap-2">
+        <h1 className="text-xl">Gabriel Kaszewski</h1>
+        <span className="flex-1" />
+        <a href="https://github.com/GKaszewski">
+          <FontAwesomeIcon icon={["fab", "github"]} />
+        </a>
+        <a href="https://www.linkedin.com/in/gabriel-kaszewski-5344b3183">
+          <FontAwesomeIcon icon={["fab", "linkedin-in"]} />
+        </a>
+      </div>
+      <div className="flex gap-2 text-sm">
+        <p className="font-semibold">
+          © Gabriel Kaszewski, {new Date().getFullYear()}. All rights reserved.
+        </p>
+        <span className="flex-1" />
+        {loggedIn && (
+          <a href="https://gabrielkaszewski.dev/api/admin">Admin page</a>
+        )}
+        <Link href="/projects">Projects</Link>
+        <Link href="https://blog.gabrielkaszewski.dev" target="_blank">
+          Blog
+        </Link>
+        {loggedIn && <Link href="/blog/editor">Editor</Link>}
+        {loggedIn && <Link href="/files">Files</Link>}
+        <Link href="/contact">Contact</Link>
+        <Link href="/login">Login</Link>
+        <a href="http://media.gabrielkaszewski.dev/">Media</a>
+      </div>
+    </div>
+  );
 }
--- src/components/navigationbar.tsx
+++ src/components/navigationbar.tsx
@@ -1,47 +1,56 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 
 export default function NavigationBar() {
-    const [nav, setNav] = useState(false);
+  const [nav, setNav] = useState(false);
 
-    useEffect(() => {
-        window.addEventListener("scroll", handleScroll);
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
 
-        return () => window.removeEventListener("scroll", handleScroll);
-    }, []);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
-    const handleScroll = () => {
-        if (window.pageYOffset > 10) {
-            if (!nav) setNav(true);
-        } else {
-            setNav(false);
-        }
-    };
+  const handleScroll = () => {
+    if (window.pageYOffset > 10) {
+      if (!nav) setNav(true);
+    } else {
+      setNav(false);
+    }
+  };
 
-    return (
-        <div
-            className={`${nav ? 'fixed flex flex-col md:flex-row w-full justify-center items-center p-4 z-20 bg-gray-900 shadow-2xl' : 'fixed flex flex-col md:flex-row w-full justify-center items-center p-4 z-20 '}`}>
-            <Link className="text-2xl" href="/">
-                Gabriel Kaszewski
-            </Link>
-            <span className="flex-1"/>
-            <div className="flex gap-4">
-                <Link className="text-lg" href="/">
-                    Home
-                </Link>
-                <Link className="text-lg" href="/projects">
-                    Projects
-                </Link>
-                <Link className="text-lg" href="/blog">
-                    Blog
-                </Link>
-                <Link className="text-lg" href="/contact">
-                    Contact
-                </Link>
-                <Link className="text-lg" href="/about">
-                    About me
-                </Link>
-            </div>
-        </div>
-    );
+  return (
+    <div
+      className={`${
+        nav
+          ? "fixed flex flex-col md:flex-row w-full justify-center items-center p-4 z-20 bg-gray-900 shadow-2xl"
+          : "fixed flex flex-col md:flex-row w-full justify-center items-center p-4 z-20 "
+      }`}
+    >
+      <Link className="text-2xl" href="/">
+        Gabriel Kaszewski
+      </Link>
+      <span className="flex-1" />
+      <div className="flex gap-4">
+        <Link className="text-lg" href="/">
+          Home
+        </Link>
+        <Link className="text-lg" href="/projects">
+          Projects
+        </Link>
+        <Link
+          className="text-lg"
+          href="https://blog.gabrielkaszewski.dev"
+          target="_blank"
+        >
+          Blog
+        </Link>
+        <Link className="text-lg" href="/contact">
+          Contact
+        </Link>
+        <Link className="text-lg" href="/about">
+          About me
+        </Link>
+      </div>
+    </div>
+  );
 }
